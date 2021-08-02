61dd7278e15c1937a9a5e9a4ca2dcef8a52cae65
Eunseong, Yeo <e7838752@gamil.com>
2021-08-02 19:01:12 +0900

    🌱 Add cycle.cpp / discriminate cycle in disjoint set

---
+++ 10. Graph Theory/cycle.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int parent[1000001];
+
+int findParent(int x) {
+  if(parent[x] == x) return x;
+  else return findParent(parent[x]);
+}
+
+void unionParent(int a, int b) {
+  a = findParent(a);
+  b = findParent(b);
+  
+  if(a < b) parent[b] = a;
+  else parent[a] = b;
+}
+
+int main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  cin>>n>>m;
+
+  for (int i = 1; i <= n; i++) {
+    parent[i] = i;
+  }
+  
+  bool cycle = false;
+  for (int i = 0; i < m; i++) {
+    int a, b;
+    cin>>a>>b;
+    
+    if (findParent(a) == findParent(b)) {
+      cycle = true;
+      break;
+    }
+    else {
+      unionParent(a, b);
+    }
+  }
+  
+  if (cycle) cout<<"Cycled";
+  else cout<<"Uncycled";
+  
+}
