46464dc2ccc894794e1ff0b9d1aec02d5ff4b0c5
Eunseong, Yeo <e7838752@gmail.com>
2021-10-05 21:27:19 +0900

    ✨ feat: Add new string problem
    Add new problem about string "compress string".

---
+++ String and Hash/01.js
@@ -0,0 +1,20 @@
+function compress(str) {
+  let answer = "";
+  str += " ";
+
+  let count = 1;
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] !== str[i + 1]) {
+      answer += str[i];
+      answer += count > 1 ? count : "";
+      count = 1;
+    }
+    else {
+      count++;
+    }
+  }
+
+  return answer;
+}
+
+console.log(compress("KKHSSSSSSSE"));
