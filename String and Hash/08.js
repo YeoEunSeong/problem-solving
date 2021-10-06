3cc5c5caad821037a3a45f9d3698f3b6f45b89dd
Eunseong, Yeo <e7838752@gmail.com>
2021-10-06 10:04:41 +0900

    ✨ feat: Add string hash problem 08.js

---
+++ String and Hash/08.js
@@ -0,0 +1,20 @@
+function classLeader(str) {
+  let result = new Map();
+
+  for (const x of str) {
+    result.set(x, (result.get(x) || 0) + 1);
+    // if (votes.get(x))
+  }
+
+  let answer;
+  let votes = Number.MIN_SAFE_INTEGER;
+  for (const [key, value] of result) {
+    if (votes < value) {
+      answer = key;
+      votes = value;
+    }
+  }
+  return answer;
+}
+
+console.log(classLeader("BACBACCACCBDEDE"));
