49ed447c64ccc9e59fcfa880e76715c71a95cb0f
Eunseong, Yeo <e7838752@gmail.com>
2021-10-06 23:48:53 +0900

    ✨ Solve 09.js with hash map
    Set hash with char(key) and number of appearance(value).
    Compare with s2.
    If char of s2 doesn't exist in hash or value is 0 , return "NO".
    And return out of loop.

--- String and Hash/09.js
+++ String and Hash/09.js
@@ -3,7 +3,20 @@ function anagram(str1, str2) {
   str2 = str2.split('').sort().join('');
 
   return str1 === str2 ? "YES" : "NO";
+}
 
+function solution(s1, s2) {
+  let hash = new Map();
+  for (const x of s1) {
+    hash.set(x, (hash.get(x) || 0) + 1);
+  }
+
+  for (const x of s2) {
+    if (!hash.has(x) || hash.get(x) === 0) return "NO";
+    hash.set(x, hash.get(x) - 1);
+  }
+  return "YES";
 }
 
 console.log(anagram("AbaAeCe", "baeeACA"));
+console.log(solution("AbaAeCe", "baeeACA"));
