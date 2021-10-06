565afa15e96792803301f16d77fedfc86d97bc8e
Eunseong, Yeo <e7838752@gmail.com>
2021-10-06 09:48:28 +0900

    ✨ feat: Add string problem 09.js

---
+++ String and Hash/09.js
@@ -0,0 +1,9 @@
+function anagram(str1, str2) {
+  str1 = str1.split('').sort().join('');
+  str2 = str2.split('').sort().join('');
+
+  return str1 === str2 ? "YES" : "NO";
+
+}
+
+console.log(anagram("AbaAeCe", "baeeACA"));
