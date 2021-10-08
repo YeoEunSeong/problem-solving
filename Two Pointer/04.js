d25beef9f67a00ca8f087094ee94e6109beb0285
Eunseong, Yeo <e7838752@gmail.com>
2021-10-08 17:36:52 +0900

    ♻️ refactor: Remove ternary operator
    At lines 10, remove ternary operator and using or operator.
    If hash doesn't have value of key, it will return undefined.
    So, I can use Map.get() or 0 instead of ternary operator.

--- Two Pointer/04.js
+++ Two Pointer/04.js
@@ -7,8 +7,12 @@ function solution(nums, m) {
     sum += x;
     answer += sum === m ? 1 : 0;
 
-    answer += hs.has(sum - m) ? hs.get(sum - m) : 0;
+    answer += hs.get(sum - m) || 0;
     hs.set(sum, (hs.get(sum) || 0) + 1);
   }
   return answer;
 }
+
+console.log(solution([1, 2, 3, -3, 1, 2], 3));
+console.log(solution([-1, 0, 1], 0));
+console.log(solution([-1, -1, -1, 1], 0));
