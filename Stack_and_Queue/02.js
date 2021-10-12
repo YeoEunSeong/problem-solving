8052d584627afd81ffb31271dad5aa89e3692903
Eunseong, Yeo <e7838752@gmail.com>
2021-10-12 19:57:21 +0900

    ✨ feat: Solve stack example

---
+++ Stack_and_Queue/02.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  let answer;
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== ")") stack.push(s[i]);
+    while (s[i] === ")" && stack.pop() !== "(") {}
+  }
+
+  answer = stack.join("");
+  return answer;
+}
+
+console.log(solution("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
