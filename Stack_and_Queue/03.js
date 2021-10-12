9dc684eaec38e1436d94a248c10fae390398b8cc
Eunseong, Yeo <e7838752@gmail.com>
2021-10-12 20:06:17 +0900

    ✨ feat: Solve stack example

---
+++ Stack_and_Queue/03.js
@@ -0,0 +1,38 @@
+function solution(s) {
+  let answer = 0;
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let num1, num2;
+    switch (s[i]) {
+      case "+":
+        num1 = parseInt(stack.pop());
+        num2 = parseInt(stack.pop());
+        stack.push(num2 + num1);
+        break;
+      case "-":
+        num1 = parseInt(stack.pop());
+        num2 = parseInt(stack.pop());
+        stack.push(num2 - num1);
+        break;
+      case "*":
+        num1 = parseInt(stack.pop());
+        num2 = parseInt(stack.pop());
+        stack.push(num2 * num1);
+        break;
+      case "/":
+        num1 = parseInt(stack.pop());
+        num2 = parseInt(stack.pop());
+        stack.push(num2 / num1);
+        break;
+
+      default:
+        stack.push(s[i]);
+        break;
+    }
+  }
+  answer = stack.pop();
+  return answer;
+}
+
+console.log(solution("352+*9-"));
