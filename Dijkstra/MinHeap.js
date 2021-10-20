0aa3f12fdc2244839204a7fa6544ac1f07ee71db
Eunseong, Yeo <e7838752@gmail.com>
2021-10-20 16:52:47 +0900

    ✨ feat: Create pair MinHeap for dijkstra

---
+++ Dijkstra/MinHeap.js
@@ -0,0 +1,44 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+    this.heap.push(-1e9);
+  }
+  insert(a, b) {
+    this.heap.push([a, b]);
+    this.upheap(this.size());
+  }
+  upheap(pos) {
+    let tmp = this.heap[pos];
+    while (tmp[1] < this.heap[parseInt(pos / 2)][1]) {
+      this.heap[pos] = this.heap[parseInt(pos / 2)];
+      pos = parseInt(pos / 2);
+    }
+    this.heap[pos] = tmp;
+  }
+  get() {
+    if (this.empty()) return false;
+    if (this.size() == 1) return this.heap.pop();
+    let res = this.heap[1];
+    this.heap[1] = this.heap.pop();
+    this.downheap(1, this.size());
+
+    return res;
+  }
+  downheap(pos, len) {
+    let tmp = this.heap[pos];
+    while (pos <= parseInt(len / 2)) {
+      let child = pos * 2;
+      if (child < len && this.heap[child][1] > this.heap[child + 1][1]) child++;
+      if (tmp[1] <= this.heap[child][1]) break;
+      this.heap[pos] = this.heap[child];
+      pos = child;
+    }
+    this.heap[pos] = tmp;
+  }
+  size() {
+    return this.heap.length - 1;
+  }
+  empty() {
+    return this.size() === 0 ? true : false;
+  }
+}
