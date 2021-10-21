42dff52fc2e19f062e47634432fefb197267fbc0
Eunseong, Yeo <e7838752@gmail.com>
2021-10-21 21:11:55 +0900

    🩹 Merge increasing and decreasing to if statement

--- String and Hash/05.js
+++ String and Hash/05.js
@@ -1,28 +1,21 @@
 function reverseStr(str) {
   const isAlphabet = RegExp(/[a-zA-Z]/);
-  str = str.split('');
+  str = str.split("");
 
   let left = 0;
   let right = str.length - 1;
 
   while (left < right) {
     if (!isAlphabet.test(str[left])) {
-      console.log(str[left]);
       left++;
-    }
-    else if (!isAlphabet.test(str[right])) {
+    } else if (!isAlphabet.test(str[right])) {
       right--;
-    }
-    else {
+    } else {
       let temp = str[left];
-      str[left] = str[right];
-      str[right] = temp;
-      left++;
-      right--;
+      str[left++] = str[right];
+      str[right--] = temp;
     }
   }
 
-  return str.join('');  
+  return str.join("");
 }
-
-console.log(reverseStr("a#b!GE*T@S"));
