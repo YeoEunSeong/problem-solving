39a4944c4f9adc70068c920897f48b76a353df54
Eunseong, Yeo <e7838752@gmail.com>
2021-10-21 14:04:08 +0900

    ✨ Solve dp problem

--- Dynamic Programming/boj12865.js
+++ Dynamic Programming/boj12865.js
@@ -1,28 +1,22 @@
-function solutoin(nums, n, k) {
-  let answer;
+function solution(nums, k) {
+  let dy = Array(k + 1).fill(0);
 
-  let dy = Array(n).fill(0);
-
-  for (const [v, w] of nums) {
-    for (let i = n - 1; i >= w; i--) {
+  for (const [w, v] of nums) {
+    for (let i = k; i >= w; i--) {
       dy[i] = Math.max(dy[i], dy[i - w] + v);
     }
   }
-
-  console.log(dy);
-
-  return answer;
+  return dy[k];
 }
-
 console.log(
-  solutoin(
+  solution(
     [
+      [4, 7],
       [6, 13],
       [4, 8],
       [3, 6],
       [5, 12],
     ],
-    4,
     7
   )
 );
