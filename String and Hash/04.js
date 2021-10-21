093348d132edf041e4259ba3272dc93342101371
Eunseong, Yeo <e7838752@gmail.com>
2021-10-21 21:08:36 +0900

    🩹 Merge increasing and decreasing to if condition statement

--- String and Hash/04.js
+++ String and Hash/04.js
@@ -4,18 +4,12 @@ function isPalindrome1(str) {
   let right = str.length - 1;
 
   while (left < right) {
-    if (str[left] !== str[right]) return "NO";
-    left++;
-    right--;
+    if (str[left++] !== str[right--]) return "NO";
   }
   return "YES";
 }
 
 function isPalindrome2(str) {
   str = str.toLowerCase();
-  return str === str.split('').reverse().join('') ? "YES" : "NO";
+  return str === str.split("").reverse().join("") ? "YES" : "NO";
 }
-
-console.log(isPalindrome1("gooG"));
-console.log(isPalindrome2("gooG"));
-console.log(isPalindrome2("goo"));
